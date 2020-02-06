df9f9a0d8d16998dba8898e9759e7b2d5249a9bd
yak.gjen <yak.gjen@gmail.com>
2020-02-06 15:12:53 +0300

    init

---
+++ precache-manifest.babbd594ae6d39aa6b64b6235e63b4d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "906cb223aa8bbea41e0f53b5d0be4c75",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9b190efb039994e429a5",
+    "url": "./static/css/main.2aef7bec.chunk.css"
+  },
+  {
+    "revision": "1babaa95503c33281b68",
+    "url": "./static/js/2.28b78bbb.chunk.js"
+  },
+  {
+    "revision": "9b190efb039994e429a5",
+    "url": "./static/js/main.dc7e24ce.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
